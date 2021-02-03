14006db33e8165624ecbc1a79b25fa1edeef37c7
dhowe <dhowe@cims.nyu.edu>
2021-02-03 13:38:04 +0800

    delay to sleep

--- scripts/vnc-all.sh
+++ scripts/vnc-all.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 echo Opening SSH tunnels
 ssh -N -L localhost:5901:localhost:5900 $SCM1 &
@@ -8,9 +8,16 @@ ssh -N -L localhost:5904:localhost:5900 $SCM4 &
 ssh -N -L localhost:5905:localhost:5900 $SCM5 &
 ssh -N -L localhost:5906:localhost:5900 $SCM6 &
 
+sleep 3
+
 open vnc://localhost:5901
+sleep .5
 open vnc://localhost:5902
+sleep .5
 open vnc://localhost:5903
+sleep .5
 open vnc://localhost:5904
+sleep .5
 open vnc://localhost:5905
+sleep .5
 open vnc://localhost:5906
